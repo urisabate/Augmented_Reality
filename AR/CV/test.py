fa8e6d958e4bf18183d42b234091b9d6bdee588a
urisabate <36154193+urisabate@users.noreply.github.com>
2020-02-19 09:37:11 +0100

    numpy exercises finished

--- AR/CV/test.py
+++ AR/CV/test.py
@@ -2,14 +2,14 @@ import numpy as np
 
 def ex1():
     print("EX_1")
-    arr = np.zeros(shape=(1,10))
+    arr = np.zeros(shape=(1, 10))
     print(arr)
 
 ex1()
 
 def ex2():
     print("EX_2")
-    arr = np.zeros(shape=(1,10))
+    arr = np.zeros(shape=(1, 10))
     arr[ :, 4] = 5
     print(arr)
 
@@ -17,25 +17,24 @@ ex2()
 
 def ex3():
     print("EX_3")
-    arr = np.arange(10,50)
+    arr = np.arange(10, 50)
     print(arr)
 
 ex3()
 
 def ex4():
     print("EX_4")
-    arr = np.arange(1,10)
-    arr = arr.reshape((3,3))
+    arr = np.arange(1, 10)
+    arr = arr.reshape((3, 3))
     print(arr)
 
 ex4()
 
 def ex5():
     print("EX_5")
-    arr = np.arange(1,10)
-    arr = arr.reshape((3,3))
-    #flip hor missing
-    #...
+    arr = np.arange(1, 10)
+    arr = arr.reshape((3, 3))
+    arr = np.fliplr(arr)
 
     print(arr)
 
@@ -43,10 +42,9 @@ ex5()
 
 def ex6():
     print("EX_6")
-    arr = np.arange(1,10)
-    arr = arr.reshape((3,3))
-    #flip ver missing
-    #...
+    arr = np.arange(1, 10)
+    arr = arr.reshape((3, 3))
+    arr = np.flipud(arr)
 
     print(arr)
 
@@ -79,7 +77,7 @@ ex9()
 def ex10():
     print("EX_10")
     arr = np.ones(shape=(10, 10))
-    arr[1:1, 8:8] = 0
+    arr[1:9, 1:9] = 0
     #not working and don't know why :'(
     print(arr)
 
@@ -87,10 +85,10 @@ ex10()
 
 def ex11():
     print("EX_11")
-    arr = np.arange(1,6)
+    arr = np.arange(1, 6)
 
-    for i in range(1,5):        
-        arr2 = np.arange(1,6)
+    for i in range(1, 5):        
+        arr2 = np.arange(1, 6)
         arr = np.vstack((arr, arr2))
     else:
         print(arr)
@@ -100,7 +98,7 @@ ex11()
 def ex12():
     print("EX_12")
     arr = np.random.randint(-100, 100, 9)
-    arr = arr.reshape((3,3))
+    arr = arr.reshape((3, 3))
     arr = np.float64(arr)
 
     print(arr)
@@ -111,9 +109,7 @@ def ex13():
     print("EX_13")
     arr = np.random.randint(-100, 100, 25)
     arr = arr.reshape((5 ,5)) 
-    print(arr)
     av = arr.mean()
-    print(av)
     arr = arr - av
 
     print(arr)
@@ -126,7 +122,7 @@ def ex14():
     arr = arr.reshape((5 ,5)) 
     arr = np.float64(arr)
 
-    for i in range(0,5):
+    for i in range(0, 5):
         av = arr[i, :].mean()
         arr[i, :] = np.subtract(arr[i, :], av)
     else: 
@@ -135,10 +131,31 @@ def ex14():
 ex14()
 
 def ex15():
-    print("EX_14")
-    arr = np.random.randint(-100, 100, 25)
-    arr = arr.reshape((5 ,5)) 
-    arr = np.float64(arr)
+    print("EX_15")
+    value = 0.5
+    arr = np.random.random_sample((5, 5))
 
-    
+    print(arr)
+    print(arr.flat[np.abs(arr - value).argmin()])
 
+ex15() 
+
+def ex16():
+    print("EX_16")
+    arr = np.random.randint(0, 10, 9)
+    arr = arr.reshape((3 ,3))
+    count = 0
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if arr[i, j] > 5:
+                count += 1
+    else:
+        print(arr)
+        print(count)
+
+ex16()
+
+#ex17 to ex23 about images see pdf:
+#"Intro to Image Processing in Virtual Campus"
+#but first... let me take a selfie
+#and fix all the problems from previous exercises
